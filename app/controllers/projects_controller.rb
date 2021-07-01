0f60bc9be05b0e39eeb587bf5e449e0c5e8a8578
Josefa <josefa0320lara@gmail.com>
2021-06-30 21:04:37 -0400

    formulario completo primer commit

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,34 @@
+class ProjectsController < ApplicationController
+  def index
+    @selected_state ='All'
+    if params[:state].present? && params[:state] != 'All'
+      @selected_state = params[:state]
+      @projects = Project.where('state = ?', params[:state])
+    else 
+       @projects = Project.all
+    end
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    #Project.create(
+    #  name: params[:project][:name],
+    #  description:params[:project][:description],
+    #  state:params[:project][:state],
+    #  starts_on: params[:project][:starts_on],
+    #  ends_on:params[:project][:ends_on]
+    #)
+    post =  Project.new(project_params)
+    post.save
+    
+    redirect_to root_path
+  end
+
+  def project_params
+    params.require(:project).permit(:name, :description, :state, :starts_on, :ends_on)
+  end
+
+end
